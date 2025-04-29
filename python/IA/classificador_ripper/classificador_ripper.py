3561df352ff31e7dcdef6e265e26e14a3adc8d53
SaulBassoJr <saulbassoj02@gmail.com>
2025-04-29 16:16:14 -0300

    add randomness in data division from classificador_ripper.py

--- python/IA/classificador_ripper/classificador_ripper.py
+++ python/IA/classificador_ripper/classificador_ripper.py
@@ -26,7 +26,7 @@ y = y.loc[X_imputed.index].copy()
 y = y.map({'democrat': 0, 'republican': 1})
 
 # Divisão dos dados
-X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.3)
 
 # Combinação de X e y para o formato esperado pelo wittgenstein
 train_data = X_train.copy()
